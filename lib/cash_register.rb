2e690e1b57d040e158f3c382cb89ab90c8c3b39d
Luis Zabala <luis.zabala076@gmail.com>
2019-01-28 17:29:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -40,7 +40,7 @@ class CashRegister
        # turn discount into a float point
        # total minus discount
        # winning
-      "After the discount, the total comes to $#{@total}."
+      "After the discount, the total comes to $#{@total.to_i}."
     else 
       "There is no discount to apply."
   
