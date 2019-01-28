c0ea195ca959accd6175093ab9c733632b618fa3
Luis Zabala <luis.zabala076@gmail.com>
2019-01-28 17:31:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,7 +33,7 @@ class CashRegister
   def apply_discount
     if @discount!=0
        
-    @discount = @discount/100.to_i
+    @discount = @discount/100.to_f
     @total = @total-(@total*@discount)
     
        # total
