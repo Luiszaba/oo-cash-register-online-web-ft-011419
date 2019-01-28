a14da37f2e0fe5242f417b0a04dc239482e35d97
Luis Zabala <luis.zabala076@gmail.com>
2019-01-28 17:29:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,7 +34,7 @@ class CashRegister
     if @discount!=0
        
     @discount = discount/100.to_f
-    @total = total-discount
+    @total = @total-@discount
     
        # total
        # turn discount into a float point
