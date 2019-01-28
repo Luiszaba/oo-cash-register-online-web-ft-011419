2a1534a681fff83295145ac222038a345a36c6b6
Luis Zabala <luis.zabala076@gmail.com>
2019-01-28 18:21:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,6 @@ class CashRegister
     @discount= discount
     @items = []
     
-    # we initialize the CashRegister with a total (quantity*price(discount)).  We also create an array containing all future items for purchase.
   end
   
   def add_item(title, price, quantity =1)
@@ -19,35 +18,18 @@ class CashRegister
     @price = price
     @quantity = quantity
     
-    # create the read/write for title, price, and quantity.
-    
-    @last_transaction = quantity*price
-    
-    # this will save our transaction to be used when needed.  Also reduces the amount of code required to retrieve the most recent change in transaction. 
-    
-    @total = @total + @last_transaction
-    
-    # our total is now equal to the quantity of the items being purchased by the price of the item. 
-    
-    items << title
 
-    # we add the title of our goods to the items array. 
+    @last_transaction = quantity*price
+    @total = @total + @last_transaction
+    @items << title
   end
 
-
-
-  
   def apply_discount
     
     if @discount!=0
     @discount = @discount/100.to_f
     @total = @total-(@total*@discount)
-    
-       # total
-       # turn discount into a float point
-       # total minus discount
-       # winning
-       
+
       "After the discount, the total comes to $#{@total.to_i}."
     else 
       "There is no discount to apply."
